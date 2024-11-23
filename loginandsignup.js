0079319158f6599c9a929d85f5aa06d6e3e847bf
24thurs <141382951+24thurs@users.noreply.github.com>
2024-11-23 15:47:21 +0700

    Update loginandsignup.js

--- loginandsignup.js
+++ loginandsignup.js
@@ -1,14 +1,12 @@
-// Handle switching from Login to Sign Up
 document.getElementById('goToSignUp').addEventListener('click', function (event) {
-    event.preventDefault(); // Prevent default link behavior
-    document.getElementById('loginForm').style.display = 'none'; // Hide Login Form
-    document.getElementById('signUpForm').style.display = 'block'; // Show Sign Up Form
+    event.preventDefault(); //ลิ้งค์กัน
+    document.getElementById('loginForm').style.display = 'none'; 
+    document.getElementById('signUpForm').style.display = 'block'; 
   });
   
-  // Handle switching from Sign Up to Login
   document.getElementById('goToLogin').addEventListener('click', function (event) {
-    event.preventDefault(); // Prevent default link behavior
-    document.getElementById('signUpForm').style.display = 'none'; // Hide Sign Up Form
-    document.getElementById('loginForm').style.display = 'block'; // Show Login Form
+    event.preventDefault(); 
+    document.getElementById('signUpForm').style.display = 'none'; 
+    document.getElementById('loginForm').style.display = 'block'; 
   });
   
